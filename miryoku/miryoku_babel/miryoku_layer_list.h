45298e08ec619a247582187a182fc47efdddb513
Daniel Galinkin <danielgalinkin@gmail.com>
2025-07-02 07:53:46 -0300

    MIRYOKU names

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -7,16 +7,16 @@
 #if !defined (MIRYOKU_LAYER_LIST)
 
 #define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun")
+MIRYOKU_X(BASE,   "MBase") \
+MIRYOKU_X(EXTRA,  "MExtra") \
+MIRYOKU_X(TAP,    "MTap") \
+MIRYOKU_X(BUTTON, "MButton") \
+MIRYOKU_X(NAV,    "MNav") \
+MIRYOKU_X(MOUSE,  "MMouse") \
+MIRYOKU_X(MEDIA,  "MMedia") \
+MIRYOKU_X(NUM,    "MNum") \
+MIRYOKU_X(SYM,    "MSym") \
+MIRYOKU_X(FUN,    "MFun")
 
 #define U_BASE   8
 #define U_EXTRA  9
