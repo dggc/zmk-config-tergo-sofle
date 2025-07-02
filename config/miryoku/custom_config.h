5ba42b0cc8d4b6a79f7fefb8e4c16ab7446e6ce4
Daniel Galinkin <danielgalinkin@gmail.com>
2025-07-02 10:24:13 -0300

    custom miryoku

--- config/miryoku/custom_config.h
+++ config/miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS
