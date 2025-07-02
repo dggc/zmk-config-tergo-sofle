04c3862087c86bf242c160131dab4d01564f8152
Daniel Galinkin <danielgalinkin@gmail.com>
2025-07-02 07:52:22 -0300

    adding ,iryoku

---
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -0,0 +1,32 @@
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+// generated -*- buffer-read-only: t -*-
+
+#pragma once
+
+#if !defined (MIRYOKU_LAYER_LIST)
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun")
+
+#define U_BASE   8
+#define U_EXTRA  9
+#define U_TAP    10
+#define U_BUTTON 11
+#define U_NAV    12
+#define U_MOUSE  13
+#define U_MEDIA  14
+#define U_NUM    15
+#define U_SYM    16
+#define U_FUN    17
+
+#endif
