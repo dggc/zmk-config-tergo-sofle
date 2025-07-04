cf6481250d31ca2a71b81645d61cf6aff5702dd1
Daniel Galinkin <danielgalinkin@gmail.com>
2025-07-04 12:15:50 -0300

    debugromao

--- config/romao/definitions/includes.h
+++ config/romao/definitions/includes.h
@@ -12,21 +12,21 @@
 
 #include "../features/mouse.dtsi"
 
-#include "../features/accents.dtsi"
-#include "../features/shortcuts.dtsi"
-#include "../features/select.dtsi"
+// #include "../features/accents.dtsi"
+// #include "../features/shortcuts.dtsi"
+// #include "../features/select.dtsi"
 
-#ifdef MACOS
-#include "../features/macos/accents.dtsi"
-#include "../features/macos/shortcuts.dtsi"
-#include "../features/macos/select.dtsi"
-#endif
+// #ifdef MACOS
+// #include "../features/macos/accents.dtsi"
+// #include "../features/macos/shortcuts.dtsi"
+// #include "../features/macos/select.dtsi"
+// #endif
 
-#ifdef LINUX
-#include "../features/linux/accents.dtsi"
-#include "../features/linux/shortcuts.dtsi"
-#include "../features/linux/select.dtsi"
-#endif
+// #ifdef LINUX
+// #include "../features/linux/accents.dtsi"
+// #include "../features/linux/shortcuts.dtsi"
+// #include "../features/linux/select.dtsi"
+// #endif
 
 // #include "../features/sentence.dtsi"
 // #include "../features/smart.dtsi"
