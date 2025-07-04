6e1d668595c4a6086644771650283ef477e61591
Daniel Galinkin <danielgalinkin@gmail.com>
2025-07-04 11:57:14 -0300

    debugromao

--- config/romao/definitions/includes.h
+++ config/romao/definitions/includes.h
@@ -3,39 +3,39 @@
 #include <dt-bindings/zmk/modifiers.h>
 #include <dt-bindings/zmk/mouse.h>
 
-#include "helpers.h"
-#include "config.dtsi"
+// #include "helpers.h"
+// #include "config.dtsi"
 
-#if __has_include("flags.h")
-#include "flags.h"
-#endif
+// #if __has_include("flags.h")
+// #include "flags.h"
+// #endif
 
-#include "../features/mouse.dtsi"
+// #include "../features/mouse.dtsi"
 
-#include "../features/accents.dtsi"
-#include "../features/shortcuts.dtsi"
-#include "../features/select.dtsi"
+// #include "../features/accents.dtsi"
+// #include "../features/shortcuts.dtsi"
+// #include "../features/select.dtsi"
 
-#ifdef MACOS
-#include "../features/macos/accents.dtsi"
-#include "../features/macos/shortcuts.dtsi"
-#include "../features/macos/select.dtsi"
-#endif
+// #ifdef MACOS
+// #include "../features/macos/accents.dtsi"
+// #include "../features/macos/shortcuts.dtsi"
+// #include "../features/macos/select.dtsi"
+// #endif
 
-#ifdef LINUX
-#include "../features/linux/accents.dtsi"
-#include "../features/linux/shortcuts.dtsi"
-#include "../features/linux/select.dtsi"
-#endif
+// #ifdef LINUX
+// #include "../features/linux/accents.dtsi"
+// #include "../features/linux/shortcuts.dtsi"
+// #include "../features/linux/select.dtsi"
+// #endif
 
-#include "../features/sentence.dtsi"
-#include "../features/smart.dtsi"
-#include "../features/adaptive.dtsi"
-#include "../features/symbols.dtsi"
+// #include "../features/sentence.dtsi"
+// #include "../features/smart.dtsi"
+// #include "../features/adaptive.dtsi"
+// #include "../features/symbols.dtsi"
 
-#include "../features/combos.dtsi"
-#include "../features/leader.dtsi"
+// #include "../features/combos.dtsi"
+// #include "../features/leader.dtsi"
 
-#if __has_include("../secret.dtsi")
-#include "../secret.dtsi"
-#endif
+// #if __has_include("../secret.dtsi")
+// #include "../secret.dtsi"
+// #endif
