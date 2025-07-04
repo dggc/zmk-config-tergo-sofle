e7dfca598b69b880a674fa1610d8759587d27c9e
Daniel Galinkin <danielgalinkin@gmail.com>
2025-07-04 13:12:30 -0300

    debugromao

--- config/romao/definitions/includes.h
+++ config/romao/definitions/includes.h
@@ -16,25 +16,25 @@
 #include "../features/shortcuts.dtsi"
 #include "../features/select.dtsi"
 
-// #ifdef MACOS
-// #include "../features/macos/accents.dtsi"
-// #include "../features/macos/shortcuts.dtsi"
-// #include "../features/macos/select.dtsi"
-// #endif
+#ifdef MACOS
+#include "../features/macos/accents.dtsi"
+#include "../features/macos/shortcuts.dtsi"
+#include "../features/macos/select.dtsi"
+#endif
 
-// #ifdef LINUX
-// #include "../features/linux/accents.dtsi"
-// #include "../features/linux/shortcuts.dtsi"
-// #include "../features/linux/select.dtsi"
-// #endif
+#ifdef LINUX
+#include "../features/linux/accents.dtsi"
+#include "../features/linux/shortcuts.dtsi"
+#include "../features/linux/select.dtsi"
+#endif
 
 #include "../features/sentence.dtsi"
 #include "../features/smart.dtsi"
 #include "../features/adaptive.dtsi"
 #include "../features/symbols.dtsi"
 
-#include "../features/combos.dtsi"
-#include "../features/leader.dtsi"
+// #include "../features/combos.dtsi"
+// #include "../features/leader.dtsi"
 
 // #if __has_include("../secret.dtsi")
 // #include "../secret.dtsi"
