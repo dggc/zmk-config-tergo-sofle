90e2848bea8c5aec2e41bca5a38ec1d892c2b0aa
Daniel Galinkin <danielgalinkin@gmail.com>
2025-07-04 09:59:35 -0300

    debugromao

--- config/miryoku/miryoku_babel/miryoku_layer_list.h
+++ config/miryoku/miryoku_babel/miryoku_layer_list.h
@@ -18,15 +18,17 @@ MIRYOKU_X(NUM,    "MNum") \
 MIRYOKU_X(SYM,    "MSym") \
 MIRYOKU_X(FUN,    "MFun")
 
-#define U_BASE   8
-#define U_EXTRA  9
-#define U_TAP    10
-#define U_BUTTON 11
-#define U_NAV    12
-#define U_MOUSE  13
-#define U_MEDIA  14
-#define U_NUM    15
-#define U_SYM    16
-#define U_FUN    17
+#define U_LAYER_START 3
+
+#define U_BASE   (U_LAYER_START + 0)
+#define U_EXTRA  (U_LAYER_START + 1)
+#define U_TAP    (U_LAYER_START + 2)
+#define U_BUTTON (U_LAYER_START + 3)
+#define U_NAV    (U_LAYER_START + 4)
+#define U_MOUSE  (U_LAYER_START + 5)
+#define U_MEDIA  (U_LAYER_START + 6)
+#define U_NUM    (U_LAYER_START + 7)
+#define U_SYM    (U_LAYER_START + 8)
+#define U_FUN    (U_LAYER_START + 9)
 
 #endif
